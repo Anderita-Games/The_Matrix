aaf7f8a76b82e922c6b6d67707a945a8cc2521c1
Andrew Friedman <andrewfriedman01@gmail.com>
2018-06-16 01:07:12 -0400

    Added App
    This is the first version that GitHub will see of The Matrix!

---
+++ Assets/Code/Character.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+	Master Canvas;
+	Transform Physical_Me;
+	UnityEngine.UI.Text Me;
+	string Chars = "!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ][^_`abcdefghijklmnopqrstuvwxyz{|}~";
+
+	// Use this for initialization
+	void Start () {
+		if (this.gameObject.transform.position.y <= -200) {
+			Destroy(this.gameObject);
+		}
+		Canvas = GameObject.Find("Canvas").GetComponent<Master>();
+		Physical_Me = this.gameObject.transform;
+		Me = this.gameObject.GetComponent<UnityEngine.UI.Text>();
+		Me.color = Color.white;
+		Me.text = Chars[Random.Range(0, Chars.Length)].ToString();
+		StartCoroutine(Life());
+	}
+		
+	IEnumerator Life () {
+		yield return new WaitForSeconds(Canvas.Delay);
+		Me.color = Color.green;
+		GameObject Clone = Instantiate(this.gameObject, new Vector2(Physical_Me.position.x, Physical_Me.position.y), new Quaternion(0, 0, 0, 0), GameObject.Find("Canvas").transform);
+		Clone.transform.position = new Vector3(Clone.transform.position.x, Clone.transform.position.y - 40, 0);
+		for (int i = 0; i <= 10; i++) {
+			yield return new WaitForSeconds(Canvas.Delay);
+			Me.color = new Color(0, 1, 0, (255 - (i * 25.5f)) / 255);
+		}
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Code/Master.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Master : MonoBehaviour {
+	public GameObject Character;
+	public float Delay = .03f;
+	public float Speed = .1f;
+
+	int Quantity;
+
+	void Start () {
+		//Get all the characters falling
+		Quantity = Mathf.FloorToInt(this.gameObject.GetComponent<Canvas>().pixelRect.width / 18.75f);
+		StartCoroutine(Spawn_Level());
+	}
+
+	/*void update () {
+		for (int i = 0; i < Positions.Length; i++) {
+			if (Positions[i] >= 10 && Random.Range(1,10) == 2) {
+				Instantiate(Character, new Vector2(18.75f * i, -18.75f), new Quaternion(0, 0, 0, 0), this.gameObject.transform);
+			}
+		}
+	}*/
+
+	IEnumerator Spawn_Level () {
+		while (1 > 0) {
+			if (Random.Range(1,6) == 3) {
+				yield return new WaitForSeconds(Random.Range(0f, 1f));
+				Spawn(Random.Range(0, Quantity));
+			}
+		}
+	}
+
+	void Spawn (int i) {
+		GameObject Child = Instantiate(Character, new Vector2(18.75f * i, 1261.29f), new Quaternion(0, 0, 0, 0), this.gameObject.transform);
+		Child.transform.position = new Vector2(((this.gameObject.GetComponent<Canvas>().pixelRect.width / Quantity) + (18.75f / 2)) * i, 1261.29f);
+	}
+}
